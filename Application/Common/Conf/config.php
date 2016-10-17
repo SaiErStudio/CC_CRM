e527330b89a4aa7d7e5db5e941ffaa4ec7dc3b86
K55L <285251412@qq.com>
2016-10-17 14:36:02 +0800

    添加文件夹（项目目录）---wys

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    'TMPL_PARSE_STRING'=>array(
+        '__Public__' => __ROOT__.'/Public',
+        '__Css__' => __ROOT__.'/Public/css',
+        '__Js__' => __ROOT__.'/Public/js',
+        '__Image__'=>__ROOT__.'/Public/image'
+    ),
+
+);
